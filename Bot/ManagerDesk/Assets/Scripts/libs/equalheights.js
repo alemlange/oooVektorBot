f9a87c5b5ad89a7eb4e09d9705a11fd3252cfc3c
alchesnokov <thechesnokov@gmail.com>
2017-07-18 00:42:31 +0300

    fixed cards

---
+++ Bot/ManagerDesk/Assets/Scripts/libs/equalheights.js
@@ -0,0 +1,14 @@
+﻿var k = jQuery.noConflict();
+function equalHeight(group) {
+    var tallest = 0;
+    group.each(function () {
+        thisHeight = k(this).height();
+        if (thisHeight > tallest) {
+            tallest = thisHeight;
+        }
+    });
+    group.height(tallest);
+}
+k(document).ready(function () {
+    equalHeight(k(".info-item"));
+});

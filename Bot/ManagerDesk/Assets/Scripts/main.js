871df180d557fb9cd97b232a31ce11a2225d8bc7
alemlange <langealem@gmail.com>
2017-08-01 22:48:43 +0300

    fix script

--- Bot/ManagerDesk/Assets/Scripts/main.js
+++ Bot/ManagerDesk/Assets/Scripts/main.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
 
     $(".navbar-left-btn").on("click", function (slideNavbar) {
         $(".navbar-left").toggle("left");
-        $(".page-wrapper").css("padding-left", "0");
-        $(".navbar-left-btn").on("click", function () {
-            $(".navbar-left").slideDown();
-            $(".page-wrapper").css("padding-left", "220px");
-        });
+        $(".page-wrapper").css("padding-left", "220px");
+        //$(".navbar-left-btn").on("click", function () {
+        //    $(".navbar-left").slideDown();
+        //    $(".page-wrapper").css("padding-left", "220px");
+        //});
     });
 
     $(".navbar-left").on("click", function (activeItem) {

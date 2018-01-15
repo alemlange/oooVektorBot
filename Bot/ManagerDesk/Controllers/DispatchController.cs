427c49f57b927d696bf2e84c2e102a77c6c49662
alemlange <langealem@gmail.com>
2018-01-15 13:51:26 +0300

    disptach views

---
+++ Bot/ManagerDesk/Controllers/DispatchController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LiteDbService.Helpers;
+using ManagerDesk.ViewModels;
+using ManagerDesk.ViewModels.Enums;
+using AutoMapper;
+using DataModels;
+using ManagerDesk.Services;
+
+namespace ManagerDesk.Controllers
+{
+    public class DispatchController : Controller
+    {
+        [HttpGet]
+        public ActionResult AllDispatches()
+        {
+            var service = ServiceCreator.GetDispatchesService();
+            var dispatches = service.GetAllActiveDispatches();
+
+            var model = Mapper.Map<List<DispatchViewModel>>(dispatches);
+            return View("DispatchCardList", model);
+        }
+
+        [HttpGet]
+        public ActionResult EditDish(Guid dishId)
+        {
+            try
+            {
+                var service = ServiceCreator.GetManagerService(User.Identity.Name);
+
+                var dish = service.GetDish(dishId);
+                if (dish != null)
+                {
+                    var model = Mapper.Map<DishViewModel>(dish);
+
+                    var allDishes = service.GetAllDishes();
+                    if (allDishes != null && allDishes.Any())
+                    {
+                        model.AvailableCategories = allDishes.Where(o => !string.IsNullOrEmpty(o.Category)).Select(o => o.Category).Distinct().ToList();
+                    }
+
+                    return View("DishCardEdditable", model);
+                }
+                else
+                    throw new Exception("Dish not found!");
+            }
+            catch (Exception ex)
+            {
+                return Json(new { isAuthorized = true, isSuccess = false, error = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditDish(Dish dish)
+        {
+            try
+            {
+                var service = ServiceCreator.GetManagerService(User.Identity.Name);
+                if (dish.Id == Guid.Empty)
+                    service.CreateNewDish(dish);
+                else
+                    service.UpdateDish(dish);
+
+                return Json(new { isAuthorized = true, isSuccess = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { isAuthorized = true, isSuccess = false, error = ex.Message });
+            }
+        }
+    }
+}
---
+++ Bot/ManagerDesk/ViewModels/DispatchViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManagerDesk.ViewModels
+{
+    public class DispatchViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Host { get; set; }
+
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
